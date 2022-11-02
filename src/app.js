dd052ff1c88c0d1a622b701f6a4e5f7877085f61
Klippie <info@johanvanderklift.nl>
2022-11-02 18:38:27 +0100

    Fetching data from API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+import axios from "axios";
+
 console.log('Hallo daar!');
+
+async function fetchData() {
+  try {
+    const result = await axios.get('https://restcountries.com/v2/all');
+    result.data.map((country) => {
+      const { name: countryName, flags: countyFlags, population: countyPopulation } = result.data;
+      console.log(countryName, countyFlags, countyPopulation);
+    })
+
+    console.log(result.data);
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+fetchData();
+
