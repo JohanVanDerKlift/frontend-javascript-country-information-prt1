ea8c6ef596eeed8855004bb2745ec59d5b19d9e2
Klippie <info@johanvanderklift.nl>
2022-11-03 20:50:10 +0100

    finished assignment, begin on css

--- src/app.js
+++ src/app.js
@@ -5,17 +5,50 @@ console.log('Hallo daar!');
 async function fetchData() {
   try {
     const result = await axios.get('https://restcountries.com/v2/all');
-    result.data.map((country) => {
-      const { name: countryName, flags: countyFlags, population: countyPopulation } = result.data;
-      console.log(countryName, countyFlags, countyPopulation);
-    })
-
     console.log(result.data);
-
+    return result.data;
   } catch (e) {
     console.error(e);
   }
 }
 
-fetchData();
+async function showData() {
+  let data = await fetchData();
+  data.sort((a, b) => {
+    return a.population - b.population;
+  })
+  data.map((country) => {
+    const countryToPrint = document.getElementById('country-list');
+    countryToPrint.innerHTML += `
+        <li class="${getRegionColor(country.region)}"><img class="flag" src="${country.flags.svg}" alt="Country flag"><span class="country-name">${country.name}</span>
+        <span class="population black">Has a population of ${country.population}</span></li>
+    `;
+  })
+}
 
+function getRegionColor(region) {
+  let color = "";
+  switch (region) {
+    case "Asia":
+      color = "red";
+      break;
+    case "Africa":
+      color = "blue"
+      break;
+    case "Americas":
+      color = "green";
+      break;
+    case "Europe":
+      color = "yellow";
+      break;
+    case "Oceania":
+      color = "purple";
+      break;
+    default:
+      color = "black";
+      break;
+  }
+  return color;
+}
+
+showData();
