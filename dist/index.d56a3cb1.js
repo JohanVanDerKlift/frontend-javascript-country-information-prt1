745d55d9aa9533d624385daaa619beb935a117f6
Klippie <info@johanvanderklift.nl>
2022-11-14 18:11:13 +0100

    Add some styling

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -462,8 +462,6 @@ function hmrAcceptRun(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
-// console.log('Hallo daar!');
-// Country information part 1
 const URI = "https://restcountries.com/v2/";
 const ENDPOINT_ALL = "all";
 async function fetchData() {
@@ -512,24 +510,7 @@ function getRegionColor(region) {
     }
     return color;
 }
-showData(); // Country information part 2
- // async function fetchCountry(name) {
- //   const ENDPOINT_NAME = `name/${name}`;
- //   try {
- //     const country = await axios.get(URI + ENDPOINT_NAME)
- //   } catch (e) {
- //     console.error(e)
- //   }
- // }
- //
- // const btn = document.getElementById("button");
- // const countryName = document.getElementById("country-name");
- //
- // btn.addEventListener("click", () => {
- //   fetchCountry(countryName.value);
- // })
- //
- // console.log(countryName);
+showData();
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
